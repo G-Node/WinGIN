7386e39f74436e096b9f62700e9e31719e523a71
Vanek <vanek@bio.lmu.de>
2019-02-25 15:57:04 +0100

    Installer - Changed file name of json with installation errors

--- InstallerLibrary/Installer1.cs
+++ InstallerLibrary/Installer1.cs
@@ -145,7 +145,7 @@ namespace InstallerLibrary
             }
             catch (Exception exc)
             {
-                var fs = File.CreateText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\exc.json");
+                var fs = File.CreateText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\GinError.json");
                 fs.Write(JsonConvert.SerializeObject(exc));
                 fs.Flush();
                 fs.Close();
