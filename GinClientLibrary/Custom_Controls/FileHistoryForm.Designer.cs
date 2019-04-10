31e6c5d250729f226c853015fe04575b0b30bae3
Vanek <vanek@bio.lmu.de>
2019-04-10 14:43:04 +0200

    Small visual change in file history form

--- GinClientLibrary/Custom_Controls/FileHistoryForm.Designer.cs
+++ GinClientLibrary/Custom_Controls/FileHistoryForm.Designer.cs
@@ -82,7 +82,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Location = new System.Drawing.Point(154, 409);
+            this.button2.Location = new System.Drawing.Point(144, 414);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(108, 58);
             this.button2.TabIndex = 2;
@@ -93,7 +93,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(24, 409);
+            this.button1.Location = new System.Drawing.Point(24, 414);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 58);
             this.button1.TabIndex = 1;
