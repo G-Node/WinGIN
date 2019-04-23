72a7c8ab823ce9dc4292af9abfe93c5e520cf198
Vanek <vanek@bio.lmu.de>
2019-04-23 16:09:02 +0200

    Added comments moved information strings to variables

--- GinClientLibrary/Custom_Controls/FileHistoryForm.Designer.cs
+++ GinClientLibrary/Custom_Controls/FileHistoryForm.Designer.cs
@@ -88,7 +88,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Cancel_Click);
             // 
             // button1
             // 
@@ -99,7 +99,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Restore";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Ok_Click);
             // 
             // FileHistoryForm
             // 
--- GinClientLibrary/Custom_Controls/FileHistoryForm.cs
+++ GinClientLibrary/Custom_Controls/FileHistoryForm.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 
 namespace GinClientLibrary.Custom_Controls
 {
+    /// <summary>
+    /// Form used to show user available file version for checkout
+    /// </summary>
     public partial class FileHistoryForm : Form
     {
         public string hashRestore = "";
@@ -11,6 +14,7 @@ namespace GinClientLibrary.Custom_Controls
         {
             InitializeComponent();
             listView1.Items.Clear();
+            ///show loaded files history versions ins listView
             foreach (var history in fileHistory)
             {
                 listView1.Items.Add(new ListViewItem(new[] { history.getDateTime().ToString("dd/MM/yyyy HH:mm:ss"), history.authorname, history.subject, history.body, history.abbrevhash }));
@@ -18,7 +22,7 @@ namespace GinClientLibrary.Custom_Controls
             listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Ok_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.OK;
             if (listView1.SelectedItems.Count == 1)
@@ -28,7 +32,7 @@ namespace GinClientLibrary.Custom_Controls
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Cancel_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.Cancel;
             Close();
