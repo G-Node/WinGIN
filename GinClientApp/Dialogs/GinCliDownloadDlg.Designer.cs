4a23aa71c2b261d8c7bc87f35e0d405dd1eff66b
vanek2 <vanek2@rti.zcu.cz>
2020-10-24 18:21:43 +0200

    Added new dialog for download, extraction and showing installation progress of gin-cli program (preparation for moving gin-cli install from setup)

---
+++ GinClientApp/Dialogs/GinCliDownloadDlg.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GinClientApp.Dialogs
+{
+    partial class GinCliDownloadDlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GinCliDownloadDlg));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 56);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(296, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gin Cli installation in progress... Downloading...";
+            this.label1.UseWaitCursor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // GinCliDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 232);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GinCliDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gin-Cli Installation";
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ GinClientApp/Dialogs/GinCliDownloadDlg.cs
@@ -0,0 +1,135 @@
+﻿using Ionic.Zip;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace GinClientApp.Dialogs
+{
+
+    public partial class GinCliDownloadDlg : Form
+    {
+        public string path;
+        private string gincli;
+        private static readonly string _ginURL =
+          "https://github.com/G-Node/gin-cli/releases/download/v1.11/gin-cli-1.11-windows32.zip";
+        /// <summary>
+        /// gin-cli for windows 34 bit that was tested
+        /// </summary>
+        private static readonly string _gin64URL =
+           "https://github.com/G-Node/gin-cli/releases/download/v1.11/gin-cli-1.11-windows64.zip";
+        public GinCliDownloadDlg()
+        {
+            InitializeComponent();
+            var appDataPath = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\g-node\WinGIN\gin-cli\");
+            path = appDataPath.FullName;
+            DownloadGinCli();
+
+        }
+
+        private void Wb_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+            label1.Text = "Download Complete. Extracting... Please wait...";
+            label1.Refresh();
+
+            this.backgroundWorker1.RunWorkerAsync();
+                      
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var archive = new ZipFile(gincli))
+            {
+                archive.ExtractProgress += ZipProgressChanged;
+                archive.ExtractAll(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\g-node\WinGIN\gin-cli\",ExtractExistingFileAction.OverwriteSilently);
+            }
+            
+
+        }
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else
+            {
+            File.Delete(gincli);
+            Close();
+
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {         
+            progressBar1.Value = e.ProgressPercentage;   
+        }
+
+        
+        private void WbOnDownloadProgressChanged(object sender,
+            DownloadProgressChangedEventArgs downloadProgressChangedEventArgs)
+        {
+            progressBar1.Value = downloadProgressChangedEventArgs.ProgressPercentage;
+        }
+
+
+        private void ZipProgressChanged(object sender,
+                ExtractProgressEventArgs zipProgressChangedEventArgs)
+        {
+            float percent;
+            float total = zipProgressChangedEventArgs.EntriesTotal;
+            if (zipProgressChangedEventArgs.EntriesExtracted != 0)
+            {
+                percent = (zipProgressChangedEventArgs.EntriesExtracted / total) * 100;
+                int done = Convert.ToInt32(Math.Floor(percent));
+                this.backgroundWorker1.ReportProgress(done);
+            }
+
+        }
+        public void DownloadGinCli()
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+                Directory.CreateDirectory(path);
+
+                var web = new WebClient();
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                web.Headers.Add("user-agent", "archive_download");
+                web.DownloadFileCompleted += new AsyncCompletedEventHandler(Wb_DownloadFileCompleted);
+                web.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WbOnDownloadProgressChanged);
+                //Download the current gin-cli release and unpack it into our install directory
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    ///64bit
+                    gincli = path + "gin-cli-latest-windows-amd64.zip";
+                    web.DownloadFileAsync(new Uri(_gin64URL), gincli);
+                }
+                else
+                {
+                    ///32 bit                  
+                    gincli = path + "gin-cli-latest-windows-386.zip";
+                    web.DownloadFileAsync(new Uri(_ginURL), gincli);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+
+
+}
+
+
+ 
+        
+   
+
+
