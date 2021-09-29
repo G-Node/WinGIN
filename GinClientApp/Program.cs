17701b46e97b48abed1a9f6724005350d0a96d6a
vanek2 <vanek2@rti.zcu.cz>
2021-09-29 10:25:14 +0200

    Several string adjustments

--- GinClientApp/Program.cs
+++ GinClientApp/Program.cs
@@ -51,11 +51,12 @@ namespace GinClientApp
         /// <summary>
         /// error messages
         /// </summary>
-        private const string connectionError = "Cannot connect to G-Node server.";
+        private const string connectionError = "Cannot connect to G-Node server. Unable to check latest Gin-Cli version.";
         private const string dokanNotInstalled = "Dokan library is missing or unsupported version is installed! Dokan is necessary for WinGIN to work. \nDo you want to install "+ dokanApp + " now?";
         private const string oldDokanInstalled = "Unsupported Dokan library is installed! Dokan 1.4.0 is necessary for WinGIN to work. Please uninstall old version.";
         private const string winginIsRunning = "WinGIN is already running.";
         private const string ginCliInstallError = "Error during gin-cli installation.";
+        private const string appName = "WinGIN";
         #endregion
 
         /// <summary>
@@ -92,8 +93,8 @@ namespace GinClientApp
                 {
                     ///if new version available ask for installation
                     var result = System.Windows.MessageBox.Show(
-                        "A new version " + remoteVersion + " of WinGIN is available. Do you want to update it now?",
-                        "WinGIN", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        "A new version " + remoteVersion + " of "+appName+" is available. Do you want to update it now?",
+                        appName, MessageBoxButton.YesNo, MessageBoxImage.Question);
 
                     if (result == MessageBoxResult.Yes)
                     {
@@ -131,13 +132,13 @@ namespace GinClientApp
                     if (CheckInstalled(dokan))
                     {
                         ///installed old dokan. Show warning and exit.
-                        MessageBox.Show(oldDokanInstalled, "WinGIN", MessageBoxButton.OK, MessageBoxImage.Error);
+                        MessageBox.Show(oldDokanInstalled, appName, MessageBoxButton.OK, MessageBoxImage.Error);
                         dokanResult = MessageBoxResult.No;
                         return;
                     }
                 }
                 ///No dokan installed,  ask for installation
-                dokanResult = MessageBox.Show(dokanNotInstalled, "WinGIN", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                dokanResult = MessageBox.Show(dokanNotInstalled, appName, MessageBoxButton.YesNo, MessageBoxImage.Warning);
                 if (dokanResult == MessageBoxResult.Yes)
                 {
                     ///try to install dokan
